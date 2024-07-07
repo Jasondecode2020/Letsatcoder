ef4bd3ac9f06f089988197a15cc8a2156ac03ac9
Zhenjie Zhang <zhenjie@campfire.com.au>
2024-07-08 09:33:50 +1000

    atcoder

---
+++ atcoder/163/361/B.py
@@ -0,0 +1,55 @@
+standard_input, packages = 1, 1
+
+if 1:
+  if standard_input:
+    import io, os, sys
+    input = lambda: sys.stdin.readline().strip()
+
+    import math
+    inf = math.inf
+
+    def S(): # string
+      return input()
+    
+    def I(): # int
+      return int(input())
+
+    def II(): # iterate int from a list
+      return map(int, input().split())
+
+    def LS(): # list of string
+      return list(input().split())
+
+    def LI(): # list of int
+      return list(map(int, input().split()))
+
+    def LF(): # list of float
+      return list(map(float, input().split()))
+
+    def M10(): # iterate of 1 to 0
+      return map(lambda x: int(x) - 1, input().split())
+
+    def L10(): # list of 1 to 0
+      return list(map(lambda x: int(x) - 1, input().split()))
+
+  if packages:
+    import random
+    import bisect
+    import typing
+    from collections import Counter, defaultdict, deque
+    from copy import deepcopy
+    from functools import cmp_to_key, lru_cache, reduce
+    from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+    from itertools import accumulate, combinations, permutations, count, product
+    from operator import add, iand, ior, itemgetter, mul, xor
+    from string import ascii_lowercase, ascii_uppercase, ascii_letters
+
+arr = []
+a, b, c, d, e, f = LI()
+a1, b1, c1, d1, e1, f1 = LI()
+res = 0
+if (a1 >= d or b1 >= e) or (a1 >= d or c1 >= f) or (b1 >= e or c1 >= f) or (d1 <= a or e1 <= b) or (d1 <= a or f1 <= c) or (e1 <= b or f1 <= c):
+  arr.append('No')
+else:
+  arr.append('Yes')
+print('\n'.join(arr))
